b48bc2b79723b44d9392275d340ebba33973965a
Stanislav <stasvitalich1994@gmail.com>
2022-09-21 00:29:39 +0700

    Задача с поиском числа в массиве не дописана до конца. ПРоблемы с типом данных bool, сам не додумался

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Задайте массив. Напишите программу, которая определяет,
+//присутствует ли заданное число в массиве.
+//Пример1: 4; массив [6, 7, 19, 345, 3] -> нет
+//Пример2: 3; массив [6, 7, 19, 345, 3] -> да
+
+Console.WriteLine("Введите количество элементов массива: ");
+int elementsCount = int.Parse(Console.ReadLine());
+int [] NewArray = new int [elementsCount];
+
+//Заполняем наш массив вручную
+for(int i = 0; i < NewArray.Length; i = i + 1)
+{
+    Console.WriteLine($"Введите элемент массива с индексом {i}");
+    NewArray[i] = int.Parse(Console.ReadLine());
+}
+
+
+//Выводим введённый ранее массив
+Console.WriteLine();
+Console.Write("Вывод массива: ");
+for (int i = 0; i < NewArray.Length; i = i + 1)
+{   
+    Console.Write($"{NewArray[i]}, ");
+}
+
+
+Console.WriteLine();
+Console.WriteLine ("Введите число для поиска в массиве: ");
+int FindNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+for (int i = 0; i < NewArray.Length; i = i + 1)
+{   
+    if (NewArray[i] == FindNumber)
+    {
+        Console.WriteLine($"Да, число {FindNumber} присутствует в введённом вами массиве");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Нет, число {FindNumber} не присутствует в введённом вами массиве");
+    }
+}
