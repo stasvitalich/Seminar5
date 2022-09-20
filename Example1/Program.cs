c038b54ce9d77dbc8b3967493f19224fd4b557da
Stanislav <stasvitalich1994@gmail.com>
2022-09-20 19:38:28 +0700

    Example1 was resolved. Example2 folder was created

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+//Найдите сумму отрицательных и положительных элементов массива.
+
+//Пишем метод, который на вход принимает массив, стартовое и финальное значение.
+int [] GetArray(int size, int startValue, int endValue)
+{   
+    var array = new int[size];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i = i + 1)
+    {
+        array[i] = random.Next(startValue, endValue + 1);
+    }
+    return array;
+}
+
+//Пишем метод, который находит сумму положительных элементов массива
+int GetSumPositiveElements(int [] array)
+{
+    var sum = 0;
+    for (int i = 0; i < array.Length; i = i + 1)
+    {
+        if (array[i] > 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+//Пишем метод, который находит сумму отрицательных элементов массива
+int GetSumNegativeNumbers(int [] array)
+{
+    var sum = 0;
+    for (int i = 0; i < array.Length; i = i + 1)
+    {
+        if (array[i] < 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+
+int [] array = GetArray(12, -9, 9);
+int sumPositive = GetSumPositiveElements(array);
+int sumNegative = GetSumNegativeNumbers(array);
+Console.WriteLine($"Cумма положительных чисел равна {sumPositive}");
+Console.WriteLine($"Cумма отрицательных чисел равна {sumNegative}");
